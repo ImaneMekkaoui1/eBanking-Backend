e48325bd23c46c4bd0c75656cd6cebf3c21f2980
ImaneMekkaoui <mekkaouiimane722@gmail.com>
2026-05-06 16:20:39 +0100

    firstPart

---
+++ src/main/java/org/mk/ebankingbackend/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package org.mk.ebankingbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.mk.ebankingbackend.enums.AccountStatus;
+
+import java.util.Date;
+import java.util.List;
+@Entity
+@Data @NoArgsConstructor
+@AllArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private Date CreatedAt;
+    private Double balance;
+    private AccountStatus AccStatus;
+    private String Currency;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation>accountOperations;
+
+}
