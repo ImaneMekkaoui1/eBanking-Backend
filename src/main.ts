e0752350a2fd78222eb765c126ab3ad191e3bd00
ImaneMekkaoui <mekkaouiimane722@gmail.com>
2026-05-09 17:18:32 +0100

    Add Angular frontend

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
+
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
